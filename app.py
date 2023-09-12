83116600bbca4fd62abc706f91fba57256ba5199
Nirnay Behera <nirnay.1behera@gmail.com>
2023-09-13 01:51:35 +0530

    compatible with BLIP and working captions fixed

--- app.py
+++ app.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+from image2promptCPU import generateCaption
 
 app = Flask(__name__)
 
-@app.route('/image2prompt')
+CORS(app)
+
+load_dotenv()
+
+Backend_Base_url = "http://localhost:8000/uploads/"
+
+@app.route('/image2prompt', methods=['POST'])
 def image2prompt():
-    
+    data = request.json.get('data')
+    imgUrls = []
+
+    for imgName in data:
+        imgUrl = Backend_Base_url + imgName
+        imgUrls.append(imgUrl)
+
+    if imgUrls:
+        generatedCaptions = generateCaption(imgUrls)
+        print(generatedCaptions)
+
+    return jsonify(generatedCaptions)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- image2promptCPU.py
+++ image2promptCPU.py
@@ -2,19 +2,21 @@ import requests
 from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
-processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
-model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+processor = BlipProcessor.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
 
-img_url = 'http://127.0.0.1:8000/uploads/2f24de8e5ea7a30948933767f8437f33.jpg' 
-raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+# img_url = 'http://127.0.0.1:8000/uploads/2f24de8e5ea7a30948933767f8437f33.jpg'
+# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-# conditional image captioning
-text = "a photography of"
-inputs = processor(raw_image, text, return_tensors="pt")
+# # conditional image captioning
+# text = "a photography of"
+# inputs = processor(raw_image, text, return_tensors="pt")
 
-out = model.generate(**inputs)
-print(processor.decode(out[0], skip_special_tokens=True))
-# >>> a photography of a woman and her dog
+# out = model.generate(**inputs)
+# print(processor.decode(out[0], skip_special_tokens=True))
+# # >>> a photography of a woman and her dog
 
 # unconditional image captioning
 # inputs = processor(raw_image, return_tensors="pt")
@@ -25,16 +27,19 @@ print(processor.decode(out[0], skip_special_tokens=True))
 
 def generateCaption(img_urls):
     captions = []
-    
-    for img_url in img_urls:
-        raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
 
-        inputs = processor(raw_image, return_tensors="pt")
+    if img_urls is not None:
+        for img_url in img_urls:
+            print("Processing")
+            raw_image = Image.open(requests.get(
+                img_url, stream=True).raw).convert('RGB')
 
-        out = model.generate(**inputs)
+            inputs = processor(raw_image, return_tensors="pt")
 
-        caption = processor.decode(out[0], skip_special_tokens=True)
+            out = model.generate(**inputs)
 
-        captions.append(caption)
+            caption = processor.decode(out[0], skip_special_tokens=True)
+
+            captions.append(caption)
 
     return captions
